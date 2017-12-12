adb95115c8f77644ba125438981c676742f105a1
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-12-12 10:38:48 +0700

    4.props

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import Header from './components/Header';
+import Body from './components/Body';
+
 export default ()=> (
-  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>Hello Dumbways.id</Text>
+  <View style={{flex: 1, justifyContent: 'center'}}>
+    <Header title="Not To Do List" color="yellow"/>
+    <Body/>
   </View>
 );
