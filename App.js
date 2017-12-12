1ce95b52643c52d22a9d2786d9acfc79a9179abb
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-12-12 11:18:30 +0700

    6.styles

--- App.js
+++ App.js
@@ -1,22 +1,63 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
+import Header from './components/Header';
+import Body from './components/Body';
 
 export default class App extends Component{
 
-  handleClick(){
-    alert('Clicked!');
-  }
-
   render(){
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableOpacity
-          onPress={()=> this.handleClick()}
-          style={{backgroundColor: '#D0D0D0', padding: 10}}
-        >
-          <Text>Click Me!</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+
+        <View style={styles.header}>
+          <Text>Header</Text>
+        </View>
+
+        <View style={styles.body}>
+
+          <View style={styles.list}>
+
+            <View style={styles.listItem}>
+              <View style={styles.listItemLeft}>
+                <Text>v</Text>
+              </View>
+              <View style={styles.listItemRight}>
+                <Text>First Item</Text>
+              </View>
+            </View>
+
+          </View>
+
+        </View>
+
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    backgroundColor: '#D0D0D0',
+    marginTop: 20,
+    flex: 0.5
+  },
+  body: {
+    backgroundColor: '#EC463E',
+    flex: 5
+  },
+  listItem: {
+    flexDirection: 'row',
+    padding: 10,
+    backgroundColor: '#FFF'
+  },
+  listItemLeft: {
+    flex: 1
+  },
+  listItemRight: {
+    flex: 9
+  }
+});
