a7969b6b66845682ac994dc02cf9e766f3694080
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-12-16 11:56:17 +0700

    13.router

--- App.js
+++ App.js
@@ -1,77 +1,21 @@
-import React, {Component} from 'react';
-import {
-  Container, Content, Text,
-  Header, Body, Left, Right,
-  List, ListItem, CheckBox, Fab, Icon
-} from 'native-base';
-import {FlatList} from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
-import TodoItem from './components/TodoItem';
+import Todos from './screens/Todos';
+import TodosCreate from './screens/TodosCreate';
 
-export default class App extends Component{
+const App = StackNavigator({
+  Todos: {
+    screen: Todos,
+    navigationOptions: {
+      headerTitle: 'Not To Do List',
+    },
+  },
+  TodosCreate: {
+    screen: TodosCreate,
+    navigationOptions: {
+      headerTitle: 'Create Todos',
+    },
+  },
+});
 
-  constructor(){
-    super();
-    this.state = {
-      count: 0,
-      todos: []
-    };
-  }
-
-  componentWillMount(){
-    console.log('componentWillMount');
-  }
-
-  componentDidMount(){
-    console.log('componentDidMount');
-    this.handleIncTodo();
-  }
-
-  _keyExtractor = (item, index) => item.id;
-
-  handleIncTodo(){
-    const count = this.state.count + 1;
-
-    let todos = this.state.todos;
-    todos.push({
-      id: count,
-      todo: "Not To Do #" + count
-    });
-
-    this.setState({
-      todos,
-      count
-    });
-  }
-
-  render(){
-    return (
-      <Container>
-        <Header>
-          <Body>
-            <Text>Not To Do List ({this.state.count})</Text>
-          </Body>
-        </Header>
-
-        <Content>
-          <List>
-            <FlatList
-              data={this.state.todos}
-              keyExtractor={this._keyExtractor}
-              renderItem={({item}) => <TodoItem todo={item}/>}
-            />
-            {/*this.todos.map((todo)=> <TodoItem todo={todo} key={todo.id}/> )*/}
-          </List>
-        </Content>
-
-        <Fab
-          style={{ backgroundColor: '#5067FF' }}
-          position="bottomRight"
-          onPress={() => this.handleIncTodo() }>
-          <Icon name="add" />
-        </Fab>
-
-      </Container>
-    );
-  }
-}
+export default App;
---
+++ screens/Todos.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {
+  Container, Content, Text,
+  Header, Body, Left, Right,
+  List, ListItem, CheckBox, Fab, Icon
+} from 'native-base';
+import {FlatList} from 'react-native';
+
+import TodoItem from '../components/TodoItem';
+
+export default class Todos extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+      todos: []
+    };
+  }
+
+  _keyExtractor = (item, index) => item.id;
+
+  render(){
+    return (
+      <Container>
+
+        <Content>
+          <List>
+            <FlatList
+              data={this.state.todos}
+              keyExtractor={this._keyExtractor}
+              renderItem={({item}) => <TodoItem todo={item}/>}
+            />
+            {/*this.todos.map((todo)=> <TodoItem todo={todo} key={todo.id}/> )*/}
+          </List>
+        </Content>
+
+        <Fab
+          style={{ backgroundColor: '#5067FF' }}
+          position="bottomRight"
+          onPress={() => this.props.navigation.navigate('TodosCreate') }>
+          <Icon name="add" />
+        </Fab>
+
+      </Container>
+    );
+  }
+}
