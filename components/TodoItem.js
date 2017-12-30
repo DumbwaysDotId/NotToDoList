29852b3619c6c8ce5ff91de36c13e33f188b4bc6
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-12-30 14:37:29 +0700

    27.delete-todos

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Body, Left, Right, ListItem, CheckBox, Text} from 'native-base';
+import {Body, Left, Right, ListItem, CheckBox, Text, SwipeRow, Button, Icon} from 'native-base';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -27,19 +28,29 @@ export default class TodoItem extends Component{
     });
   }
 
+  handleDelete(id){
+    axios.delete(`http://192.168.1.102:8000/api/todos/${id}`);
+  }
+
   render(){
     const {todo: {id, name}} = this.props;
 
     return (
-      <ListItem key={id}>
-        <Left>
-          <CheckBox checked={this.state.isDone} onPress={()=> this.handleDone()}/>
-        </Left>
-        <Body>
-          <Text>{name}</Text>
-        </Body>
-        <Right/>
-      </ListItem>
+      <SwipeRow
+        key={id}
+        rightOpenValue={-75}
+        body={
+          <View>
+            <CheckBox checked={this.state.isDone} onPress={()=> this.handleDone()}/>
+            <Text>{name}</Text>
+          </View>
+        }
+        right={
+          <Button danger onPress={() => this.handleDelete(id)}>
+            <Icon active name="trash" />
+          </Button>
+        }
+      />
     )
   }
 }
