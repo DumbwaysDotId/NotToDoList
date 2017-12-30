5e69fbd31b7f210828a015835788d6539f25af3c
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-12-30 13:47:27 +0700

    26.patch-todos

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,8 +1,31 @@
 import React, {Component} from 'react';
 import {Body, Left, Right, ListItem, CheckBox, Text} from 'native-base';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 export default class TodoItem extends Component{
+
+  state = {
+    isDone: false
+  }
+
+  componentDidMount(){
+    const {todo: {isDone}} = this.props;
+
+    this.setState({isDone: Boolean(isDone)});
+  }
+
+  handleDone(){
+    const {todo: {id}} = this.props;
+
+    this.setState({
+      isDone: !this.state.isDone
+    });
+
+    axios.patch(`http://192.168.1.102:8000/api/todos/${id}`, {
+      isDone: this.state.isDone? 0: 1
+    });
+  }
 
   render(){
     const {todo: {id, name}} = this.props;
@@ -10,7 +33,7 @@ export default class TodoItem extends Component{
     return (
       <ListItem key={id}>
         <Left>
-          <CheckBox checked={false}/>
+          <CheckBox checked={this.state.isDone} onPress={()=> this.handleDone()}/>
         </Left>
         <Body>
           <Text>{name}</Text>
